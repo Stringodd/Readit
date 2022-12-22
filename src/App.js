1cedc87c6d6979aed6aa99a0c9f7a7867c18cefb
Eray Eroglu <eray_6960@hotmail.com>
2022-12-22 19:03:18 +0300

    Login ekranı kaldırıldı

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import { RouterProvider, BrowserRouter, Routes, Route } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import router from "./router";
-import Auth from "./Pages/Auth";
 
 function App() {
   return (
     <div className="App">
       <RouterProvider router={router} />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/auth" element={<Auth />} />
-        </Routes>
-      </BrowserRouter>
     </div>
   );
 }
